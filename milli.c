698a81ee36b6e5ac9b7ada6fca1a432a662471a9
Aakash Pahuja <aakash.pahuja@kony.com>
2018-07-16 19:54:08 +0530

    Add read timeout

--- milli.c
+++ milli.c
@@ -16,26 +16,33 @@ void enableRawMode()
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(IXON | ICRNL);
   raw.c_oflag &= ~(OPOST);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
   enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+  while (1)
   {
-    if (iscntrl(c))
-    {
-      printf("%d\r\n", c);
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+      if (iscntrl(c))
+      {
+        printf("%d\r\n", c);
+      }
+      else
+      {
+        printf("%d ('%c')\r\n", c, c);
+      }
+      if (c == 'q')
+        break;
     }
-    else
-    {
-      printf("%d ('%c')\r\n", c, c);
-    }
-  }
-  return 0;
+    return 0;
 }
