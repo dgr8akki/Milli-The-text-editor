460975c1a4cc14fb74a5f4c1204b4f67be1cce30
Aakash Pahuja <KH2272@kitspl.com>
2018-07-30 18:49:33 +0100

    Add welcome message

--- milli.c
+++ milli.c
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
+
 
 enum editorKey
 {
@@ -137,36 +139,47 @@ int getCursorPosition(int *rows, int *cols)
   return 0;
 }
 
-
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3)
-    {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-                                "Milli - The text editor v%s", MILLI_VERSION);
-      if (welcomelen > E.screencols)
-        welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding)
-      {
+    if (y >= E.numrows) {
+      if (y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Milli - The text editor v%s", MILLI_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--)
-        abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
-    }
-    else
-    {
-      abAppend(ab, "~", 1);
+    } else {
+      int len = E.row.size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row.chars, len);
     }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
   }
+}
+
+/*** file i/o ***/
+void editorOpen()
+{
+  char *line = "Hello, world!";
+  ssize_t linelen = 13;
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
 }
 
 void initEditor()
@@ -344,6 +357,7 @@ int main()
 {
   enableRawMode();
   initEditor();
+  editorOpen();
   while (1)
     {
       editorRefreshScreen();
