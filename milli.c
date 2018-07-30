8b7f5e64b1f6d235cbf372a07dabde349d621f48
Aakash Pahuja <KH2272@kitspl.com>
2018-07-30 18:58:40 +0100

    Add file read feature

--- milli.c
+++ milli.c
@@ -1,3 +1,7 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 /*** includes ***/
 #include <termios.h>
 #include <unistd.h>
@@ -143,7 +147,8 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     if (y >= E.numrows) {
-      if (y == E.screenrows / 3) {
+      if (E.numrows == 0 && y == E.screenrows / 3)
+      {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
           "Milli - The text editor v%s", MILLI_VERSION);
@@ -171,15 +176,24 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 /*** file i/o ***/
-void editorOpen()
-{
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 void initEditor()
@@ -352,12 +366,12 @@ void editorMoveCursor(int key)
 }
 
 /*** init ***/
-
-int main()
-{
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
   while (1)
     {
       editorRefreshScreen();
